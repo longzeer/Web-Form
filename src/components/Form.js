1cde0647fe79af0eaf1b8f4aee23fce2c1bbdf37
Yong <yshao@cmpa.org>
2023-06-14 15:26:55 -0400

    updated

---
+++ src/components/Form.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 在这里处理表单提交的逻辑
+    alert('Submitted: '+ name + '-' + email);
+    console.log('Submitted:', name, email);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-sm mx-auto mt-4 p-4 bg-gray-100 rounded shadow">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+          Name:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1>Welcome to the Home Page</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default HomePage;
