717d0cd85fd89929e70b97a5741bcd059b93c5b7
Yong <yshao@cmpa.org>
2023-06-15 15:53:39 -0400

    update 4

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,7 +3,7 @@ import Form from './Form';
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center flex h-screen">
       <h1>Welcome to the Home Page</h1>
       <Form />
     </div>
